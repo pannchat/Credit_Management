0946ecb58ae98df5da736cc8d0d498d71fb7c545
hooong <tjrwns0529@gmail.com>
2020-05-14 22:58:09 +0900

    base

---
+++ src/main/java/capstone/project/credit_manager/CreditManagerApplication.java
@@ -0,0 +1,13 @@
+package capstone.project.credit_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/project/credit_manager/CreditManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.project.credit_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
