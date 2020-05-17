102c45468809d242921d01f983e6bb2b9cc8dc1d
hooong <tjrwns0529@gmail.com>
2020-05-17 16:50:59 +0900

    add entity Manager

--- src/main/java/capstone/project/credit_manager/Domain/Manager.java
+++ src/main/java/capstone/project/credit_manager/Domain/Manager.java
@@ -1,4 +1,15 @@
 package capstone.project.credit_manager.Domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Manager {
+    @Id @GeneratedValue
+    Long id;
+
+    Integer mNum;
+    String Pw;
+    String major;
 }
