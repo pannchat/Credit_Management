ebffb2cc8d2eb883f88ea88fff7aaf1bd51228e9
hooong <tjrwns0529@gmail.com>
2020-05-17 16:53:57 +0900

    add entity GraduationRequirement

--- src/main/java/capstone/project/credit_manager/Domain/GraduationRequirement.java
+++ src/main/java/capstone/project/credit_manager/Domain/GraduationRequirement.java
@@ -1,11 +1,20 @@
 package capstone.project.credit_manager.Domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 public class GraduationRequirement {
     @Id @GeneratedValue
+    Long id;
+
+    Integer year;
+    String major;
+    String url;
 
 }
