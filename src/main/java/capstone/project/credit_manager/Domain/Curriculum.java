b42cf3af0db6df74dc2a86a74a49d65b0a01c9c6
hooong <tjrwns0529@gmail.com>
2020-05-19 23:33:24 +0900

    add Curriculum Entity

--- src/main/java/capstone/project/credit_manager/Domain/Curriculum.java
+++ src/main/java/capstone/project/credit_manager/Domain/Curriculum.java
@@ -1,4 +1,16 @@
 package capstone.project.credit_manager.Domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
 public class Curriculum {
+    @Id @GeneratedValue
+    Long id;
+
 }
